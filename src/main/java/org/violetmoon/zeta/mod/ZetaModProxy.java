542659abd8b5637d426dd1ea5a5aff9ef2e7791b
Vasco Lavos <vazkii@hotmail.com>
2024-01-08 05:38:07 +0000

    Should fix piston replacement logic issue

--- src/main/java/org/violetmoon/zeta/mod/ZetaModProxy.java
+++ src/main/java/org/violetmoon/zeta/mod/ZetaModProxy.java
@@ -2,7 +2,7 @@ package org.violetmoon.zeta.mod;
 
 import org.violetmoon.zeta.Zeta;
 import org.violetmoon.zeta.config.SyncedFlagHandler;
-import org.violetmoon.zeta.util.handler.FuelHandler;
+import org.violetmoon.zeta.config.ZetaGeneralConfig;
 import org.violetmoon.zeta.util.handler.RecipeCrawlHandler;
 import org.violetmoon.zeta.util.handler.ToolInteractionHandler;
 import org.violetmoon.zeta.world.EntitySpawnHandler;
@@ -15,7 +15,8 @@ public class ZetaModProxy {
 			.subscribe(RecipeCrawlHandler.class)
 			.subscribe(ToolInteractionHandler.class)
 			.subscribe(EntitySpawnHandler.class)
-			.subscribe(WorldGenHandler.class);
+			.subscribe(WorldGenHandler.class)
+			.subscribe(ZetaGeneralConfig.class);
 		
 		zeta.playBus
 			.subscribe(RecipeCrawlHandler.class)
