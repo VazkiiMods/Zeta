cb08b8fe56a91a088cadf3797a35303a7bc8bf40
IThundxr <contact@ithundxr.dev>
2024-01-21 15:04:10 -0500

    javadoc

--- common/src/main/java/org/violetmoon/zeta/util/ToolUtils.java
+++ common/src/main/java/org/violetmoon/zeta/util/ToolUtils.java
@@ -4,10 +4,12 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.violetmoon.zeta.mixin.mixins.AccessorAxeItem;
 import org.violetmoon.zeta.mixin.mixins.AccessorShovelItem;
 
+/*
+ * Utilities from forge patches that get added to <tool>Item usually for getting states.
+ */
 public class ToolUtils {
     public static @NotNull BlockState getAxeStrippingState(BlockState originalState) {
         Block block = AccessorAxeItem.zeta$getStrippables().get(originalState.getBlock());
