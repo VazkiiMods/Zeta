6cef76fcc07a5dd1dd4a0cb0f24bc6567f120784
Siuolplex <lmeitrodt@gmail.com>
2024-01-24 16:38:00 -0600

    "It will conflict" I literally do not care

--- forge/src/main/java/org/violetmoon/zetaimplforge/mixin/mixins/client/GameRenderMixin.java
+++ forge/src/main/java/org/violetmoon/zetaimplforge/mixin/mixins/client/GameRenderMixin.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.violetmoon.zeta.client.event.play.ZEarlyRender;
 import org.violetmoon.zeta.util.zetalist.ZetaClientList;
-import org.violetmoon.zetaimplforge.client.event.play.ForgeZEarlyRender;
 
 import net.minecraft.client.renderer.GameRenderer;
 
