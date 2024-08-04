6502e27a14d23e793cf08cb0a6f24f7cadeb6f5b
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-08-04 17:02:01 +0200

    its a ImodBusEvent now

--- src/main/java/org/violetmoon/zetaimplforge/event/load/ForgeZGatherAdditionalFlags.java
+++ src/main/java/org/violetmoon/zetaimplforge/event/load/ForgeZGatherAdditionalFlags.java
@@ -1,10 +1,11 @@
 package org.violetmoon.zetaimplforge.event.load;
 
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.IModBusEvent;
 import org.violetmoon.zeta.config.ConfigFlagManager;
 import org.violetmoon.zeta.event.load.ZGatherAdditionalFlags;
 
-public class ForgeZGatherAdditionalFlags extends Event implements ZGatherAdditionalFlags {
+public class ForgeZGatherAdditionalFlags extends Event implements ZGatherAdditionalFlags , IModBusEvent {
 
     private final ZGatherAdditionalFlags wrapped;
 
