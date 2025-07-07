8ad253cb4854074b53eb8274fce7e66b8fa5db50
Siuolplex <lmeitrodt@gmail.com>
2025-07-07 16:45:35 -0500

    Evil Z Button stops from doing stuff real
    Signed-off-by: Siuolplex <lmeitrodt@gmail.com>

--- src/main/java/org/violetmoon/zetaimplforge/mod/ZetaModForge.java
+++ src/main/java/org/violetmoon/zetaimplforge/mod/ZetaModForge.java
@@ -4,6 +4,7 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
+import org.violetmoon.zeta.Zeta;
 import org.violetmoon.zeta.client.config.widget.ZButtonHandler;
 import org.violetmoon.zeta.mod.ZetaMod;
 import org.violetmoon.zetaimplforge.EventTest;
@@ -24,7 +25,7 @@ public class ZetaModForge extends ZetaMod {
 		if(!ZETA.isProduction) {
 			ZETA.loadBus.subscribe(EventTest.class);
 			ZETA.playBus.subscribe(EventTest.class);
-			ZETA.playBus.subscribe(ZButtonHandler.class);
+			if (FMLEnvironment.dist == Dist.CLIENT) ZETA.playBus.subscribe(ZButtonHandler.class);
 		}
 	}
 
