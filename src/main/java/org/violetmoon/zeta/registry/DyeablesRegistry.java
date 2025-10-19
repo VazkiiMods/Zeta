a31deda918a99bd7bc2343e684a94d44a9fd8d95
BrokenK3yboard <63761658+BrokenK3yboard@users.noreply.github.com>
2025-10-19 10:10:50 -0500

    Fix item dye recipes

--- src/main/java/org/violetmoon/zeta/registry/DyeablesRegistry.java
+++ src/main/java/org/violetmoon/zeta/registry/DyeablesRegistry.java
@@ -1,14 +1,23 @@
 package org.violetmoon.zeta.registry;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BooleanSupplier;
-
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.FastColor;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.DyeItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.DyedItemColor;
+import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.LayeredCauldronBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.violetmoon.zeta.event.bus.LoadEvent;
 import org.violetmoon.zeta.event.load.ZRegister;
@@ -16,19 +25,10 @@ import org.violetmoon.zeta.module.ZetaModule;
 import org.violetmoon.zeta.recipe.ZetaDyeRecipe;
 import org.violetmoon.zeta.util.BooleanSuppliers;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.cauldron.CauldronInteraction;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.DyeItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.CraftingBookCategory;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.LayeredCauldronBlock;
-import net.minecraft.world.level.block.state.BlockState;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BooleanSupplier;
 
 /**
  * @see org.violetmoon.zeta.client.ClientRegistryExtension
@@ -103,56 +103,53 @@ public class DyeablesRegistry {
 		return isDyed(stack) ? stack.getComponents().get(DataComponents.DYED_COLOR) : new DyedItemColor(0xFF_FF_FF, false);
 	}
 
-	// Copy of DyeableLeatherItem
+	// Copy of DyedItemColor
 	public ItemStack dyeItem(ItemStack stack, List<DyeItem> dyes) {
-		ItemStack itemstack;
-		int[] aint = new int[3];
-		int i = 0;
-		int j = 0;
+        if (!isDyeable(stack)) {
+            return ItemStack.EMPTY;
+        } else {
+            ItemStack itemstack = stack.copyWithCount(1);
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int l = 0;
+            int i1 = 0;
+            DyedItemColor dyeditemcolor = itemstack.get(DataComponents.DYED_COLOR);
+            if (dyeditemcolor != null) {
+                int j1 = FastColor.ARGB32.red(dyeditemcolor.rgb());
+                int k1 = FastColor.ARGB32.green(dyeditemcolor.rgb());
+                int l1 = FastColor.ARGB32.blue(dyeditemcolor.rgb());
+                l += Math.max(j1, Math.max(k1, l1));
+                i += j1;
+                j += k1;
+                k += l1;
+                ++i1;
+            }
 
-		if(isDyeable(stack)) {
-			itemstack = stack.copy();
-			itemstack.setCount(1);
-			if(stack.getComponents().has(DataComponents.DYED_COLOR)) {
-				DyedItemColor k = stack.getComponents().get(DataComponents.DYED_COLOR);
-				float f = (float) (k.rgb() >> 16 & 255) / 255.0F;
-				float f1 = (float) (k.rgb() >> 8 & 255) / 255.0F;
-				float f2 = (float) (k.rgb() & 255) / 255.0F;
-				i += (int) (Math.max(f, Math.max(f1, f2)) * 255.0F);
-				aint[0] += (int) (f * 255.0F);
-				aint[1] += (int) (f1 * 255.0F);
-				aint[2] += (int) (f2 * 255.0F);
-				++j;
-			}
+            for (DyeItem dyeitem : dyes) {
+                int j3 = dyeitem.getDyeColor().getTextureDiffuseColor();
+                int i2 = FastColor.ARGB32.red(j3);
+                int j2 = FastColor.ARGB32.green(j3);
+                int k2 = FastColor.ARGB32.blue(j3);
+                l += Math.max(i2, Math.max(j2, k2));
+                i += i2;
+                j += j2;
+                k += k2;
+                ++i1;
+            }
 
-			for(DyeItem dyeItem : dyes) {
-				int diffuseColor = dyeItem.getDyeColor().getTextureDiffuseColor();
-				float f = (diffuseColor >> 16 & 255) / 255.0F;
-				float f1 = (diffuseColor >> 8 & 255) / 255.0F;
-				float f2 = (diffuseColor & 255) / 255.0F;
-				i += (int) Math.max(f2, Math.max(f, f1));
-				aint[0] += (int) f;
-				aint[1] += (int) f1;
-				aint[2] += (int) f2;
-				++j;
-			}
-
-			int j1 = aint[0] / j;
-			int k1 = aint[1] / j;
-			int l1 = aint[2] / j;
-			float f3 = (float) i / (float) j;
-			float f4 = (float) Math.max(j1, Math.max(k1, l1));
-			j1 = (int) ((float) j1 * f3 / f4);
-			k1 = (int) ((float) k1 * f3 / f4);
-			l1 = (int) ((float) l1 * f3 / f4);
-			int rgb = (j1 << 8) + k1;
-			rgb = (rgb << 8) + l1;
-			stack.set(DataComponents.DYED_COLOR, new DyedItemColor(rgb, false));
-
-			return itemstack;
-		}
-
-		return ItemStack.EMPTY;
-	}
-
+            int l2 = i / i1;
+            int i3 = j / i1;
+            int k3 = k / i1;
+            float f = (float) l / (float) i1;
+            float f1 = (float) Math.max(l2, Math.max(i3, k3));
+            l2 = (int) ((float) l2 * f / f1);
+            i3 = (int) ((float) i3 * f / f1);
+            k3 = (int) ((float) k3 * f / f1);
+            int l3 = FastColor.ARGB32.color(0, l2, i3, k3);
+            boolean flag = dyeditemcolor == null || dyeditemcolor.showInTooltip();
+            itemstack.set(DataComponents.DYED_COLOR, new DyedItemColor(l3, flag));
+            return itemstack;
+        }
+    }
 }
