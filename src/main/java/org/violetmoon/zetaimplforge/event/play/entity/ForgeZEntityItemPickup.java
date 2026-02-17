01495c68e00c034ba108f2c39be5792cbf1ad3b0
Siuolplex <lmeitrodt@gmail.com>
2026-02-17 12:40:41 -0600

    Move the pickup event to target pre instead of post pickup.
    Signed-off-by: Siuolplex <lmeitrodt@gmail.com>

--- src/main/java/org/violetmoon/zetaimplforge/event/play/entity/ForgeZEntityItemPickup.java
+++ src/main/java/org/violetmoon/zetaimplforge/event/play/entity/ForgeZEntityItemPickup.java
@@ -7,9 +7,9 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 
 public class ForgeZEntityItemPickup implements ZItemEntityPickup {
-    private final ItemEntityPickupEvent.Post e;
+    private final ItemEntityPickupEvent.Pre e;
 
-    public ForgeZEntityItemPickup(ItemEntityPickupEvent.Post e) {
+    public ForgeZEntityItemPickup(ItemEntityPickupEvent.Pre e) {
         this.e = e;
     }
 
